7fe16ad34765755673e0211b2dbee435302885ed
suvirgupta <guptasuvir@gmail.com>
2017-06-22 17:24:17 -0700

    spark SQL check

---
+++ sparksql.py
@@ -0,0 +1,14 @@
+import os
+os.environ['SPARK_HOME']= '/usr/lib/spark'
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext , Row
+
+conf = (SparkConf().setMaster("local").setAppName("MyApp").set("spark.executor.memory", "1g"))
+sc=SparkContext(conf= conf)
+
+sqlc = SQLContext(sc)
+
+tweetpath = 'hdfs://quickstart.cloudera:8020/user/cloudera/spark_sql/cache-1000000.json'
+twittertable = sqlc.read.json(tweetpath)
+twittertable.registerTempTable("twittertable")
+print sqlc.sql("select text, user.screen_name from twittertable where user.screen_name = 'realDonaldTrump' limit 10").collect()
